010b0ac52831ced6d2401fa4f3a56a03ca92ead8
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-09-25 14:25:08 +0530

    modified script

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const { app } = require('./src/app');
 
 console.log(process.env);
-app.listen(3001, () => console.log('listening on the port : 3001'));
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log('listening on the port :', port));
